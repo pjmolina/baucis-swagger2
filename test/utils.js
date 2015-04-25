c20bde10892544103503886452c2e4025a973e10
pjmolina <pjmolina@gmail.com>
2015-04-26 00:47:16 +0200

    Added unit-test for utils module

---
+++ test/utils.js
@@ -0,0 +1,17 @@
+var expect = require('expect.js');
+var utils = require('../utils');
+
+describe('Utils module', function () {
+	it('capitalize should handle null', function (done) {
+		expect(utils.capitalize(null)).to.be(null);
+		done();
+	}); 
+	it('capitalize should handle single chars', function (done) {
+		expect(utils.capitalize('a')).to.be('A');
+		done();
+	}); 
+	it('capitalize should handle longer strings', function (done) {
+		expect(utils.capitalize('abc')).to.be('Abc');
+		done();
+	}); 
+});
