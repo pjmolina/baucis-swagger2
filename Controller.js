c58906caa969d4eaf74fe353b10a50589661f68b
pjmolina <pjmolina@gmail.com>
2015-06-15 13:41:18 +0200

    Refactor: extract buildParameters related functions to a new file to encapsulate responsibilities

--- Controller.js
+++ Controller.js
@@ -3,6 +3,7 @@
 // __Dependencies__
 var mongoose = require('mongoose');
 var utils = require('./utils');
+var params = require('./parameters');
 
 // __Private Members__
 
@@ -12,186 +13,7 @@ module.exports = function () {
 
   // __Private Instance Members__
 
-  function getParamId() {
-    return {
-        name: 'id',
-        in: 'path',
-        description: 'The ID of a ' + controller.model().singular() +'.',
-        type: 'string',
-        required: true
-      };
-  }
-  function getParamXBaucisUpdateOperator() {
-    return {
-        name: 'X-Baucis-Update-Operator',
-        in: 'header',
-        description: '**BYPASSES VALIDATION** May be used with PUT to update the document using $push, $pull, or $set.',
-        type: 'string',
-        required: false
-      };
-  }
-  function getParamSkip() {
-    return {
-        name: 'skip',
-        in: 'query',
-        description: 'How many documents to skip. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#skip',
-        type: 'integer',
-        format: 'int32',
-        required: false
-      };
-  }
-  function getParamLimit() {
-    return {
-        name: 'limit',
-        in: 'query',
-        description: 'The maximum number of documents to send. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#limit',
-        type: 'integer',
-        format: 'int32',
-        required: false
-      };
-  }
-  function getParamCount() {
-    return {
-        name: 'count',
-        in: 'query',
-        description: 'Set to true to return count instead of documents. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#count',
-        type: 'boolean',
-        required: false
-      };
-  }
-  function getParamConditions() {
-    return {
-        name: 'conditions',
-        in: 'query',
-        description: 'Set the conditions used to find or remove the document(s). See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#conditions',
-        type: 'string',
-        required: false
-      };
-  }
-  function getParamSort() {
-    return {
-        name: 'sort',
-        in: 'query',
-        description: 'Set the fields by which to sort. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#sort',
-        type: 'string',
-        required: false
-      };
-  }
-  function getParamSelect() {
-    return {
-      name: 'select',
-      in: 'query',
-      description: 'Select which paths will be returned by the query. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#select',
-      type: 'string',
-      required: false
-    };
-  }  
-  function getParamPopulate() {
-    return {
-      name: 'populate',
-      in: 'query',
-      description: 'Specify which paths to populate. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#populate',
-      type: 'string',
-      required: false
-    };
-  }  
-  function getParamDistinct() {
-    return {
-        name: 'distinct',
-        in: 'query',
-        description: 'Set to a path name to retrieve an array of distinct values. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#distinct',
-        type: 'string',
-        required: false
-      };
-  }
-  function getParamHint() {
-    return {
-        name: 'hint',
-        in: 'query',
-        description: 'Add an index hint to the query (must be enabled per controller). See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#hint',
-        type: 'string',
-        required: false
-      };
-  }
-  function getParamComment() {
-    return {
-        name: 'comment',
-        in: 'query',
-        description: 'Add a comment to a query (must be enabled per controller). See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#comment',
-        type: 'string',
-        required: false
-      };
-  }
-  function getParamDocument(isPost) {
-    // TODO post body can be single or array
-    return {
-        name: 'document',
-        in: 'body',
-        description: (isPost) ?
-           'Create a document by sending the paths to be updated in the request body.' :
-           'Update a document by sending the paths to be updated in the request body.',
-        schema: {
-          $ref: '#/definitions/' + utils.capitalize(controller.model().singular()),
-        }, 
-        required: true
-      };
-  }    
-  // Generate parameter list for path
-  function generatePathParameters(isInstance) {
-    var parameters = [];
-    if (isInstance) {
-      // Parameters available for singular routes
-      parameters.push(getParamId());
-    }
-    return parameters;
-  }
-
-  // Generate parameter list for operations
-  function generateOperationParameters(isInstance, verb) {
-    var parameters = [];
-    // Parameters available for singular and plural routes
-    parameters.push(getParamSelect(), 
-                    getParamPopulate());
-
-    addSingularParameters(isInstance, parameters);
-    addCollectionParameters(isInstance, parameters);
-    addPostParameters(verb, parameters);
-    addPutParameters(verb, parameters);
-
-    return parameters;
-  }
-
-  function addSingularParameters(isInstance, parameters) {
-    if (isInstance) {
-      // Parameters available for singular routes
-      parameters.push(getParamXBaucisUpdateOperator());
-    }
-  }
-  function addCollectionParameters(isInstance, parameters) {
-    if (!isInstance) {
-      // Parameters available for plural routes
-      parameters.push(getParamSkip(),
-                      getParamLimit(),
-                      getParamCount(),
-                      getParamConditions(),
-                      getParamSort(),
-                      getParamDistinct(),
-                      getParamHint(),
-                      getParamComment()
-                      );      
-    }
-  }
-  function addPostParameters(verb, parameters) {
-    if (verb === 'post') {
-      parameters.push(getParamDocument(true));
-    }
-  }
-  function addPutParameters(verb, parameters) {
-    if (verb === 'put') {
-      parameters.push(getParamDocument(false));
-    }
-  }
-
+  
   function buildTags(resourceName) {
     return [ resourceName ];
   }
@@ -258,13 +80,13 @@ module.exports = function () {
     res.description = description;
     return res;
   }
-  function buildBaseOperation(mode, verb, resourceName, pluralName) {
+  function buildBaseOperation(mode, verb, resourceName, pluralName, controller) {
     var isInstance = (mode === 'instance');
 	var resourceKey = utils.capitalize(resourceName);
     var res = {
       //consumes: ['application/json'], //if used overrides global definition
       //produces: ['application/json'], //if used overrides global definition
-      parameters: generateOperationParameters(isInstance, verb),
+      parameters: params.generateOperationParameters(isInstance, verb, controller),
       responses: buildResponsesFor(isInstance, verb, resourceName, pluralName)
     };
 	var sec = buildSecurityFor();
@@ -273,42 +95,42 @@ module.exports = function () {
 	}
     if (isInstance) {
       if ('get' === verb) {
-		return buildOperationInfo(res, 
-				   'get' + resourceKey + 'ById',
-				   'Get a ' + resourceName + ' by its unique ID',
-				   'Retrieve a ' + resourceName + ' by its ID' + '.');
+    		return buildOperationInfo(res, 
+    				   'get' + resourceKey + 'ById',
+    				   'Get a ' + resourceName + ' by its unique ID',
+    				   'Retrieve a ' + resourceName + ' by its ID' + '.');
       } 
       else if ('put' === verb) {
-		return buildOperationInfo(res, 
-				   'update' + resourceKey,
-				   'Modify a ' + resourceName + ' by its unique ID',
-				   'Update an existing ' + resourceName + ' by its ID' + '.');
+    		return buildOperationInfo(res, 
+    				   'update' + resourceKey,
+    				   'Modify a ' + resourceName + ' by its unique ID',
+    				   'Update an existing ' + resourceName + ' by its ID' + '.');
       }    
       else if ('delete' === verb) {
-		return buildOperationInfo(res, 
-				   'delete' + resourceKey + 'ById',
-				   'Delete a ' + resourceName + ' by its unique ID',
-				   'Deletes an existing ' + resourceName + ' by its ID' + '.');
+    		return buildOperationInfo(res, 
+    				   'delete' + resourceKey + 'ById',
+    				   'Delete a ' + resourceName + ' by its unique ID',
+    				   'Deletes an existing ' + resourceName + ' by its ID' + '.');
       }        
     } else {
       //collection
       if ('get' === verb) {
-		return buildOperationInfo(res, 
-				   'query' + resourceKey,
-				   'Query some ' + pluralName,
-				   'Query over ' + pluralName + '.');
+    		return buildOperationInfo(res, 
+    				   'query' + resourceKey,
+    				   'Query some ' + pluralName,
+    				   'Query over ' + pluralName + '.');
       } 
       else if ('post' === verb) {
-		return buildOperationInfo(res, 
-				   'create' + resourceKey,
-				   'Create some ' + pluralName,
-				   'Create one or more ' + pluralName + '.');
+    		return buildOperationInfo(res, 
+    				   'create' + resourceKey,
+    				   'Create some ' + pluralName,
+    				   'Create one or more ' + pluralName + '.');
       }    
       else if ('delete' === verb) {
-		return buildOperationInfo(res, 
-				   'delete' + resourceKey + 'ByQuery',
-				   'Delete some ' + pluralName + ' by query',
-				   'Delete all ' + pluralName + ' matching the specified query.');
+    		return buildOperationInfo(res, 
+    				   'delete' + resourceKey + 'ByQuery',
+    				   'Delete some ' + pluralName + ' by query',
+    				   'Delete all ' + pluralName + ' matching the specified query.');
       }      
     }
     return res;
@@ -318,7 +140,7 @@ module.exports = function () {
     var resourceName = controller.model().singular();
     var pluralName = controller.model().plural();
 
-    var operation = buildBaseOperation(mode, verb, resourceName, pluralName);
+    var operation = buildBaseOperation(mode, verb, resourceName, pluralName, controller);
     operation.tags = buildTags(resourceName);
     containerPath[verb] = operation;
     return operation;
@@ -500,7 +322,7 @@ module.exports = function () {
   } 
 
   // A method used to generate a Swagger model definition for a controller
-  function generateModelDefinition (schema, definitionName) {
+  function generateModelDefinition(schema, definitionName) {
     var definition = {
       required: [],
       properties: {}
@@ -560,7 +382,7 @@ module.exports = function () {
 
     var paths = {};
     paths[instancePath] = {
-      parameters : generatePathParameters(true)
+      parameters : params.generatePathParameters(true, controller)
     };
     paths[collectionPath] = {};
     buildOperation(paths[instancePath], 'instance', 'get');
---
+++ parameters.js
@@ -0,0 +1,188 @@
+//Module with helper functions for building Swagger parameters metadata 
+var utils = require('./utils');
+
+function getParamId(controller) {
+    return {
+        name: 'id',
+        in: 'path',
+        description: 'The ID of a ' + controller.model().singular() +'.',
+        type: 'string',
+        required: true
+      };
+  }
+  function getParamXBaucisUpdateOperator() {
+    return {
+        name: 'X-Baucis-Update-Operator',
+        in: 'header',
+        description: '**BYPASSES VALIDATION** May be used with PUT to update the document using $push, $pull, or $set.',
+        type: 'string',
+        required: false
+      };
+  }
+  function getParamSkip() {
+    return {
+        name: 'skip',
+        in: 'query',
+        description: 'How many documents to skip. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#skip',
+        type: 'integer',
+        format: 'int32',
+        required: false
+      };
+  }
+  function getParamLimit() {
+    return {
+        name: 'limit',
+        in: 'query',
+        description: 'The maximum number of documents to send. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#limit',
+        type: 'integer',
+        format: 'int32',
+        required: false
+      };
+  }
+  function getParamCount() {
+    return {
+        name: 'count',
+        in: 'query',
+        description: 'Set to true to return count instead of documents. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#count',
+        type: 'boolean',
+        required: false
+      };
+  }
+  function getParamConditions() {
+    return {
+        name: 'conditions',
+        in: 'query',
+        description: 'Set the conditions used to find or remove the document(s). See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#conditions',
+        type: 'string',
+        required: false
+      };
+  }
+  function getParamSort() {
+    return {
+        name: 'sort',
+        in: 'query',
+        description: 'Set the fields by which to sort. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#sort',
+        type: 'string',
+        required: false
+      };
+  }
+  function getParamSelect() {
+    return {
+      name: 'select',
+      in: 'query',
+      description: 'Select which paths will be returned by the query. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#select',
+      type: 'string',
+      required: false
+    };
+  }  
+  function getParamPopulate() {
+    return {
+      name: 'populate',
+      in: 'query',
+      description: 'Specify which paths to populate. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#populate',
+      type: 'string',
+      required: false
+    };
+  }  
+  function getParamDistinct() {
+    return {
+        name: 'distinct',
+        in: 'query',
+        description: 'Set to a path name to retrieve an array of distinct values. See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#distinct',
+        type: 'string',
+        required: false
+      };
+  }
+  function getParamHint() {
+    return {
+        name: 'hint',
+        in: 'query',
+        description: 'Add an index hint to the query (must be enabled per controller). See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#hint',
+        type: 'string',
+        required: false
+      };
+  }
+  function getParamComment() {
+    return {
+        name: 'comment',
+        in: 'query',
+        description: 'Add a comment to a query (must be enabled per controller). See doc: https://github.com/wprl/baucis/wiki/Query-String-Parameters#comment',
+        type: 'string',
+        required: false
+      };
+  }
+  function getParamDocument(isPost, controller) {
+    // TODO post body can be single or array
+    return {
+        name: 'document',
+        in: 'body',
+        description: (isPost) ?
+           'Create a document by sending the paths to be updated in the request body.' :
+           'Update a document by sending the paths to be updated in the request body.',
+        schema: {
+          $ref: '#/definitions/' + utils.capitalize(controller.model().singular()),
+        }, 
+        required: true
+      };
+  }    
+  // Generate parameter list for path
+  function generatePathParameters(isInstance, controller) {
+    var parameters = [];
+    if (isInstance) {
+      // Parameters available for singular routes
+      parameters.push(getParamId(controller));
+    }
+    return parameters;
+  }
+
+  // Generate parameter list for operations
+  function generateOperationParameters(isInstance, verb, controller) {
+    var parameters = [];
+    // Parameters available for singular and plural routes
+    parameters.push(getParamSelect(), 
+                    getParamPopulate());
+
+    addSingularParameters(isInstance, parameters);
+    addCollectionParameters(isInstance, parameters);
+    addPostParameters(verb, controller, parameters);
+    addPutParameters(verb, controller, parameters);
+
+    return parameters;
+  }
+
+  function addSingularParameters(isInstance, parameters) {
+    if (isInstance) {
+      // Parameters available for singular routes
+      parameters.push(getParamXBaucisUpdateOperator());
+    }
+  }
+  function addCollectionParameters(isInstance, parameters) {
+    if (!isInstance) {
+      // Parameters available for plural routes
+      parameters.push(getParamSkip(),
+                      getParamLimit(),
+                      getParamCount(),
+                      getParamConditions(),
+                      getParamSort(),
+                      getParamDistinct(),
+                      getParamHint(),
+                      getParamComment()
+                      );      
+    }
+  }
+  function addPostParameters(verb, controller, parameters) {
+    if (verb === 'post') {
+      parameters.push(getParamDocument(true, controller));
+    }
+  }
+  function addPutParameters(verb, controller, parameters) {
+    if (verb === 'put') {
+      parameters.push(getParamDocument(false, controller));
+    }
+  }
+
+
+module.exports = {
+	generateOperationParameters : generateOperationParameters,
+	generatePathParameters : generatePathParameters,
+};
